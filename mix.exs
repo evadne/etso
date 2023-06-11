febdeb166316a971bb8b8358ae13ff8c84f6b9d8
Chase Granberry <chase@logflare.app>
2023-06-11 07:57:58 -0700

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Etso.MixProject do
   def project do
     [
       app: :etso,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
